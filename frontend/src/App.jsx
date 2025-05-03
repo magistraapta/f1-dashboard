ab6e9feca9c896a6acab0db7b3c040b2c0044e65
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2025-05-03 21:20:25 +0800

    fix: home page view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,26 +2,34 @@ import React, {useState} from 'react'
 import TeamStandings from './components/TeamStandings';
 import DriverStandings from './components/DriverStandings';
 import Races from './components/Races';
+import './styles/animation.css'
 
 const App = () => {
   const years = Array.from({ length: 6 }, (_, i) => 2020 + i);
-  const [selectedYear, setSelectedYear] = useState(years[4]); // Default to first year
+  const [selectedYear, setSelectedYear] = useState(years[5]); // Default to first year
 
   return (
     <div className='mt-4 flex flex-col items-center h-full mb-4'>
-      <h1 className='text-5xl font-bold mb-4'>Formula 1 Dashboard</h1>
-      <div className='flex items-center'>
-        <p className='p-2'>Select season: </p>
-        <select 
-            className="mb-4 p-2 border border-gray-200 rounded-lg"
-            value={selectedYear}
-            onChange={(e) => setSelectedYear(Number(e.target.value))}
-        >
-            {years.map((year) => (
-                <option key={year} value={year}>{year}</option>
-            ))}
-        </select>
+      <div className='flex p-8 w-11/12 justify-between'>
+        <div>
+          <h1 className='text-5xl font-bold mb-4'>Dashboard</h1>
+          <p className='text-gray-400'>2025 Season Overview</p>
+        </div>
+        
+        <div className='flex items-center mb-4'>
+          {/* <p className='p-2'>Select season: </p> */}
+          <select 
+              className="p-2 border  border-gray-400 rounded-md"
+              value={selectedYear}
+              onChange={(e) => setSelectedYear(Number(e.target.value))}
+          >
+              {years.map((year) => (
+                  <option key={year} value={year}>{year}</option>
+              ))}
+          </select>
+        </div>
       </div>
+      
       
 
       <div className='grid grid-cols-2 gap-4 justify-center'>
@@ -34,9 +42,5 @@ const App = () => {
     </div>
   );
 };
-
-
-
-
 
 export default App
